3a6650fbee26ecf43f009a2152b0b1205a99ff11
Emmanuel Sam <emnlsam873@gmail.com>
2024-05-12 14:36:52 +0530

    class created to send attendance data with name

--- src/main/java/dev/emnl/empregbackend/dto/AttendanceDto.java
+++ src/main/java/dev/emnl/empregbackend/dto/AttendanceDto.java
@@ -1,4 +1,16 @@
 package dev.emnl.empregbackend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class AttendanceDto {
+    private Integer id;
+    private String name;
+    private Integer work;
+    private Integer overtime;
+    private Integer advance;
 }
