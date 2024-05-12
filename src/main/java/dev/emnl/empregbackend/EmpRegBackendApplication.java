2af3972f03f1e271629a210b912034e32cf43303
Emmanuel Sam <emnlsam873@gmail.com>
2024-05-12 12:35:24 +0530

    Initial Commit

---
+++ src/main/java/dev/emnl/empregbackend/EmpRegBackendApplication.java
@@ -0,0 +1,30 @@
+package dev.emnl.empregbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class EmpRegBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpRegBackendApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.allowCredentials(false).maxAge(3600);
+			}
+		};
+	}
+}
---
+++ src/test/java/dev/emnl/empregbackend/EmpRegBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.emnl.empregbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpRegBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
