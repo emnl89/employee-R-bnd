3773f833fe2907f628695427e55a6d1c6b29732c
Emmanuel Sam <emnlsam873@gmail.com>
2024-05-12 15:19:33 +0530

    class excluding attendance list created

--- src/main/java/dev/emnl/empregbackend/dto/EmployeeDto.java
+++ src/main/java/dev/emnl/empregbackend/dto/EmployeeDto.java
@@ -1,4 +1,17 @@
 package dev.emnl.empregbackend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmployeeDto {
+
+    private Integer id;
+    private String name;
+    private Integer wageRate;
+    private Integer overtimeRate;
+    private Integer dueAmount;
 }
