4bcfa2d4a2869820e18c97574caa052a7605c452
Emmanuel Sam <emnlsam873@gmail.com>
2024-05-12 15:21:44 +0530

    user database created

--- src/main/java/dev/emnl/empregbackend/model/UserEntity.java
+++ src/main/java/dev/emnl/empregbackend/model/UserEntity.java
@@ -1,4 +1,28 @@
 package dev.emnl.empregbackend.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Document(collection = "users")
+@Data
+@NoArgsConstructor
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String userName;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable( name = "user_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private List<Role> roles = new ArrayList<>();
 }
--- src/main/java/dev/emnl/empregbackend/repository/UserRepository.java
+++ src/main/java/dev/emnl/empregbackend/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package dev.emnl.empregbackend.repository;
 
-public interface UserRepository {
+import dev.emnl.empregbackend.model.UserEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<UserEntity, Integer> {
+
+    public Optional<UserEntity> findByUserName(String userName);
+
+    public Boolean existsByUserName(String userName);
 }
