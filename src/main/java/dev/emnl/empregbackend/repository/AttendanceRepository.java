cebc4ec382d747384d15d94f513cff73483f99a7
Emmanuel Sam <emnlsam873@gmail.com>
2024-05-12 14:38:10 +0530

    functions to fetch all or specific data wrt date implemented

--- src/main/java/dev/emnl/empregbackend/repository/AttendanceRepository.java
+++ src/main/java/dev/emnl/empregbackend/repository/AttendanceRepository.java
@@ -1,4 +1,14 @@
 package dev.emnl.empregbackend.repository;
 
-public interface AttendanceRepository {
+import dev.emnl.empregbackend.model.Attendance;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Date;
+import java.util.List;
+
+public interface AttendanceRepository extends MongoRepository<Attendance, ObjectId> {
+    public List<Attendance> findAllByDate(Date date);
+
+    public List<Integer> findEmpIdByDate(Date day);
 }
