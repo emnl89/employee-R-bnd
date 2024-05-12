c45b174c1da1c7fbb1dd2d6c79484a25f69fe254
Emmanuel Sam <emnlsam873@gmail.com>
2024-05-12 14:38:44 +0530

    create and read functions implemented

--- src/main/java/dev/emnl/empregbackend/service/AttendanceService.java
+++ src/main/java/dev/emnl/empregbackend/service/AttendanceService.java
@@ -1,4 +1,15 @@
 package dev.emnl.empregbackend.service;
 
+import dev.emnl.empregbackend.dto.AttendanceDto;
+import dev.emnl.empregbackend.model.Attendance;
+
+import java.util.Date;
+import java.util.List;
+
 public interface AttendanceService {
+    public List<AttendanceDto> fetchAttendance(Date date);
+
+    public Attendance createAttendance(Attendance attendance);
+
+    public List<Integer> getEmployeeIds(Date day);
 }
